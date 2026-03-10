63f3963687d1511ff9ec231a6bc36262010d2a5a
dgordenin <dgordenin@gmail.com>
2026-03-10 19:33:18 +0300

    Unpack only packed files.

--- unpack.sh
+++ unpack.sh
@@ -3,16 +3,16 @@
 set -euo pipefail
 
 find ./library -type f -name "*.yut" -print0 | while IFS= read -r -d '' file; do
-    echo "Unpacking: $file"
-    
-    # создаём временный файл
-    tmp="${file}.tmp_unpacked"
-    
-    # распаковка
-    gzip -dc "$file" > "$tmp"
-    
-    # заменяем исходный файл
-    mv "$tmp" "$file"
+    if gzip -t "$file" 2>/dev/null; then
+        echo "Unpacking: $file"
+
+        tmp="${file}.tmp_unpacked"
+
+        gzip -dc "$file" > "$tmp"
+        mv "$tmp" "$file"
+    else
+        echo "Skipping (not gzip): $file"
+    fi
 done
 
 echo "Done."
